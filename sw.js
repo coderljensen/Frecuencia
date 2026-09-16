71d2032ce755327767e3da386c2a1063454d948f
coderljensen <leje0416@gmail.com>
2026-09-16 15:10:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "frecuencia-v3";
+const CACHE = "frecuencia-v4";
 const ASSETS = [
   "./", "index.html",
   "words.js", "words2.js", "words3.js", "words4.js", "words5.js", "words6.js",
